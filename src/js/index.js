3b1b49d597ee5834238939791485bceab9b02817
Cesar <cesarramez8@gmail.com>
2022-10-17 19:56:22 -0500

    Adding localStorage in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,21 @@ const prefersColor = window.matchMedia('(prefers-color-scheme: dark)')
 
 
 btnChangeMode.addEventListener('click', () => {
+    let addTheme
     
     if(prefersColor.matches) {
         document.body.classList.toggle('lightMode')
+        addTheme = document.body.classList.contains('lightMode') ? 'light' : ''
     }
 
+    localStorage.setItem('theme', addTheme)
 })
+
+
+const getTheme = localStorage.getItem('theme')
+
+if(getTheme === 'light') {
+    document.body.classList.add('lightMode')
+} else {
+    localStorage.removeItem('theme')
+}
